24c384c72c5739d0bfee8f19f93ef5a26ff1ef52
sakirsyarian <sakirsyarian@gmail.com>
2023-02-23 10:49:10 +0700

    refactor: Client

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import Login from './components/Login.vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+  components: {
+    Login,
+    Navbar,
+    Sidebar
+  },
+}
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+
+  <main class="container mx-auto">
+    <div class="grid grid-cols-5">
+      <Sidebar />
+      <Login />
+    </div>
+  </main>
+</template>
